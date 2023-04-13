d95df5a6a5555ad483ea0ac58f534449ab1eaf9a
Ethan Kaley <ethan.kaley@emc.com>
2023-04-13 09:59:14 -0400

    Create Diagrams

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/ekaley/ele548.git
 
 go 1.20
+
+require github.com/wcharczuk/go-chart/v2 v2.1.0
+
+require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.0.0-20220902085622-e7cb96979f69 // indirect
+)
--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	"log"
 	"os"
 	"strings"
+
+	// "gonum.org/v1/plot"
+	// "gonum.org/v1/plot/plotter"
+	// "gonum.org/v1/plot/plotutil"
+	// "gonum.org/v1/plot/vg"
+	"github.com/wcharczuk/go-chart/v2"
 )
 
 func main() {
@@ -18,7 +24,14 @@ func main() {
 	// for i, line := range lines {
 	// 	fmt.Println(i, line)
 	// }
-	parseInstructions(lines)
+	instr, err := parseInstructions(lines)
+	if err != nil {
+		panic(err)
+	}
+	err = plotting(instr)
+	if err != nil {
+		panic(err)
+	}
 }
 
 // read line by line into memory
@@ -41,20 +54,117 @@ func readLines(path string) ([]string, error) {
 func parseInstructions(lines []string) (map[string]int, error) {
 	instructions := make(map[string]int)
 
-	for i, line := range lines {
-		log.Printf("instruction line#%d: %s", i, line)
+	for _, line := range lines {
+		// log.Printf("instruction line#%d: %s", i, line)
 		trimmedLine := strings.TrimSpace(line)
 		instrLine := strings.Fields(trimmedLine)
 		if len(instrLine) > 0 {
-			log.Printf("riscv ISA: %s", instrLine[0])
+			// log.Printf("riscv ISA: %s", instrLine[0])
 			if instrLine[0] != "" {
-				instructions[instrLine[0]]++
+				if !strings.HasPrefix(instrLine[0], "_") {
+					if !strings.HasPrefix(instrLine[0], ".") {
+						if !strings.Contains(instrLine[0], ":") {
+							instructions[instrLine[0]]++
+						}
+					}
+				}
 			}
 		}
 
 	}
 
-	log.Printf("map %+v\n", instructions)
+	log.Printf("instructions %+v\n", instructions)
 
 	return instructions, nil
 }
+
+func plotting(instructions map[string]int) error {
+	vals := []chart.Value{}
+	for k, v := range instructions {
+		vals = append(vals, chart.Value{
+			Label: k,
+			Value: float64(v),
+		})
+	}
+
+	pie := chart.PieChart{
+		Width:  2048,
+		Height: 2048,
+		Values: vals,
+	}
+
+	p, _ := os.Create("output-pie.png")
+	defer p.Close()
+	err := pie.Render(chart.PNG, p)
+	if err != nil {
+		return err
+	}
+
+	graph := chart.BarChart{
+		Title: "Instr riscv64 ISA",
+		Background: chart.Style{
+			Padding: chart.Box{
+				Top: 40,
+			},
+		},
+		Height:   512,
+		BarWidth: 60,
+		Bars:     vals,
+	}
+
+	b, _ := os.Create("output-bar.png")
+	defer b.Close()
+	err = graph.Render(chart.PNG, b)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// func plotting(instructions map[string]int) error {
+// 	progA := plotter.Values{}
+// 	for k, v := range instructions {
+
+// 	}
+
+// 	p := plot.New()
+
+// 	p.Title.Text = "Bar chart"
+// 	p.Y.Label.Text = "Heights"
+
+// 	w := vg.Points(20)
+
+// 	barsA, err := plotter.NewBarChart(groupA, w)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	barsA.LineStyle.Width = vg.Length(0)
+// 	barsA.Color = plotutil.Color(0)
+// 	barsA.Offset = -w
+
+// 	barsB, err := plotter.NewBarChart(groupB, w)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	barsB.LineStyle.Width = vg.Length(0)
+// 	barsB.Color = plotutil.Color(1)
+
+// 	barsC, err := plotter.NewBarChart(groupC, w)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	barsC.LineStyle.Width = vg.Length(0)
+// 	barsC.Color = plotutil.Color(2)
+// 	barsC.Offset = w
+
+// 	p.Add(barsA, barsB, barsC)
+// 	p.Legend.Add("Group A", barsA)
+// 	p.Legend.Add("Group B", barsB)
+// 	p.Legend.Add("Group C", barsC)
+// 	p.Legend.Top = true
+// 	p.NominalX("One", "Two", "Three", "Four", "Five")
+
+// 	if err := p.Save(5*vg.Inch, 3*vg.Inch, "barchart.png"); err != nil {
+// 		panic(err)
+// 	}
+// }
