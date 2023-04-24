b09dea175df94188558d5263ce9359c5c01baeb7
dionisopoulosd <129804710+dionisopoulosd@users.noreply.github.com>
2023-04-23 21:29:56 -0400

    CSV_Plotter does the following:
    1.) Has a file dialog to select the CSV file ( there's a slight bug which I can't fix where the dialog windows stays open when the plot shows up)
    
    2.) The data is sorted in ascending order for the instruction frequency
    
    3.) There's a legend that shows the total of instructions and the sum of all the instances.
    
    To Do: Just need to add a second plot to show the differences beteen x86 and x64 and update the plot titles to show the file name for comparison

--- CSV_Plotter.py
+++ CSV_Plotter.py
@@ -1,24 +1,39 @@
-import pandas as ps
 import matplotlib.pyplot as plt
-import numpy as np
 import csv
+import easygui
 
-with open('/Users/David/Desktop/ele548/dater.csv') as csvfile:
-        readCSV = list(csv.reader(csvfile, delimiter=','))
+# Show the file dialog box and get the selected file path
+file_path = easygui.fileopenbox()
+
+# Reads the csv file
+with open(file_path) as csvfile:
+    readCSV = list(csv.reader(csvfile, delimiter=','))
+
+# Retrieves the indivitual rows and put them into lists
 instructionRow = readCSV[0]
 frequencyRow = readCSV[1]
 
+# Convert frequencyRow to integers, sort, and get the corresponding permutation of indices
+permutation = sorted(range(len(frequencyRow)), key=lambda i: int(frequencyRow[i]))
+sortedFrequencyRow = [frequencyRow[i] for i in permutation]
+sortedInstructionRow = [instructionRow[i] for i in permutation]
 
-#print(instructionRow)
-#print(frequencyRow)
-
-#length = len(instructionRow)
-#ind = np.arange(length)
-
-plt.figure(figsize=(15,4))
-plt.bar(instructionRow,frequencyRow)
+# Sets the figure size and plots the data for the bar graph
+plt.figure(1,figsize=(15, 4))
+plt.bar(sortedInstructionRow, sortedFrequencyRow)
 plt.grid(linestyle='--', axis='y')
 plt.xlabel('Instructions')
 plt.ylabel('Instances')
-plt.title('Instruction Usage')
+plt.title('Instruction Usage') #Will update to add the file name to the title
+
+# Add legend with total number of elements in instructionRow and sum of values in sortedFrequencyRow
+num_elements = len(instructionRow)
+sum_values = sum(int(x) for x in sortedFrequencyRow)  # Convert each element to int before summing
+legend_text = f'Total number of instructions: {num_elements}\nTotal number of instruction calls: {sum_values}'
+plt.legend([legend_text])
+
+#will use this to plot the other figure
+plt.figure(2)
+
+#show the plot
 plt.show()
